97a8c9e248f12e52bd55b17be9466691a0af3a33
elvis1056 <john800116@hotmail.com>
2019-03-24 23:50:04 +0800

    Updates

---
+++ precache-manifest.6456c095828e42fdb4d05fa9d0a39a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3246b9fae11572143fe7",
+    "url": "/React_Todo/static/js/runtime~main.88b15c87.js"
+  },
+  {
+    "revision": "7a8ec690bd9cca76a110",
+    "url": "/React_Todo/static/js/main.d6dcfdd2.chunk.js"
+  },
+  {
+    "revision": "31630e992b87f5202887",
+    "url": "/React_Todo/static/js/2.8664eb13.chunk.js"
+  },
+  {
+    "revision": "7a8ec690bd9cca76a110",
+    "url": "/React_Todo/static/css/main.f0633a66.chunk.css"
+  },
+  {
+    "revision": "a0438232636759ec492b3c38ad312892",
+    "url": "/React_Todo/index.html"
+  }
+];
